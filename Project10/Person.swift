841e8996b8fdb5de0d5a265c406c6fc16d57852a
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-09-22 23:14:15 +0300

    Initial Commit

---
+++ Project10/Person.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class Person: NSObject, Codable {
+	var name: String
+	var image: String
+
+	init(name: String, image: String) {
+		self.name = name
+		self.image = image
+	}
+}
---
+++ Project10/PersonCell.swift
@@ -0,0 +1,8 @@
+
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+}
